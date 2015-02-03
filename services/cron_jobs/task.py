680d8e4ef5d9b85772583b4580120338427c71d5
haidaoxiaofei <haidaoxiaofei@gmail.com>
2015-02-03 15:06:40 +0800

    save

--- services/cron_jobs/task.py
+++ services/cron_jobs/task.py
@@ -199,9 +199,9 @@ def gen_testing_tasks():
 
 def run():
     c = CronTab(
-        Event(gen_taking_picture, name='firebird', min=[0, 30], hour=range(10, 23)),
-        Event(gen_canteen_menus, name='menu', min=[0, 49], hour=[11, 17]),
-        Event(gen_testing_tasks, name='testing', min=[0, 49], hour=[11, 17]),
+        # Event(gen_taking_picture, name='firebird', min=[0, 30], hour=range(10, 23)),
+        # Event(gen_canteen_menus, name='menu', min=[0, 49], hour=[11, 17]),
+        Event(gen_testing_tasks, name='testing', min=[0, 15, 30, 45], hour=range(15, 19)),
     )
     c.run()
     pass
