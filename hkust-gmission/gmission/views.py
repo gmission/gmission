a0d59c27e92247b1f1cb285ebda41629b2ba0fe2
haidaoxiaofei <haidaoxiaofei@gmail.com>
2015-02-09 13:07:41 +0800

    save

--- hkust-gmission/gmission/views.py
+++ hkust-gmission/gmission/views.py
@@ -53,7 +53,7 @@ def test():
 
 @app.route('/getAnswerMessage', methods=['POST'])
 def getPersonById():
-    personId = (int)(request.form['id'])
+    personId = (int)(request.form['value'])
     print personId
     return personId
 
