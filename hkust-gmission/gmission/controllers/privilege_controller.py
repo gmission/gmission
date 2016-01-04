d74b7d6cc606fc9a997d82f2371a20c36724cfcb
Rui Fu <freelz@gmail.com>
2016-01-04 17:08:37 +0800

    fix user privilege

--- hkust-gmission/gmission/controllers/privilege_controller.py
+++ hkust-gmission/gmission/controllers/privilege_controller.py
@@ -108,10 +108,10 @@ def init_priv_table():
         push_priv_rule('/rest/position_trace/<instid>', ['GET','POST', 'PUT', 'DELETE'], priv(role=[role_admin]))
 
         push_priv_rule('/rest/role', ['GET', 'POST', 'PUT', 'DELETE'], priv(role=[role_admin]))
-        push_priv_rule('/rest/role/<instid>', ['GET','POST', 'PUT', 'DELETE'], priv(role=role_everyone))
+        push_priv_rule('/rest/role/<instid>', ['GET','POST', 'PUT', 'DELETE'], priv(role=[role_admin]))
 
         push_priv_rule('/rest/user', ['GET', 'POST', 'PUT', 'DELETE'], priv(role=[role_admin]))
-        push_priv_rule('/rest/user/<instid>', ['GET','POST', 'PUT', 'DELETE'], priv(role=[role_admin]))
+        push_priv_rule('/rest/user/<instid>', ['GET','POST', 'PUT', 'DELETE'], priv(role=role_everyone))
 
         push_priv_rule('/rest/user_last_position', ['GET', 'POST', 'PUT', 'DELETE'], priv(role=[role_admin]))
         push_priv_rule('/rest/user_last_position/<instid>', ['GET','POST', 'PUT', 'DELETE'], priv(role=[role_admin]))
