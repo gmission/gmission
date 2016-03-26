25089e33d4cc3f665af957aeda687b188bde6612
haidaoxiaofei <haidaoxiaofei@gmail.com>
2016-03-26 13:37:18 +0800

    save

--- hkust-gmission/gmission/models/extra.py
+++ hkust-gmission/gmission/models/extra.py
@@ -6,16 +6,16 @@ from base import *
 class Extra(db.Model, BasicModelMixin):
     id = db.Column(db.Integer, primary_key=True)
 
-    hit_id = db.Column(db.Integer, db.ForeignKey('hit.id'), nullable=true)
+    hit_id = db.Column(db.Integer, db.ForeignKey('hit.id'), nullable=True)
     hit = db.relationship('Hit', foreign_keys=hit_id)
 
-    answer_id = db.Column(db.Integer, db.ForeignKey('answer.id'), nullable=true)
+    answer_id = db.Column(db.Integer, db.ForeignKey('answer.id'), nullable=True)
     answer = db.relationship('Answer', foreign_keys=answer_id)
 
-    campaign_id = db.Column(db.Integer, db.ForeignKey('campaign.id'), nullable=true)
+    campaign_id = db.Column(db.Integer, db.ForeignKey('campaign.id'), nullable=True)
     campaign = db.relationship('Campaign', foreign_keys=campaign_id)
 
-    message_id = db.Column(db.Integer, db.ForeignKey('message.id'), nullable=true)
+    message_id = db.Column(db.Integer, db.ForeignKey('message.id'), nullable=True)
     message = db.relationship('Message', foreign_keys=message_id)
 
     created_on = db.Column(db.DateTime, default=datetime.datetime.now)
